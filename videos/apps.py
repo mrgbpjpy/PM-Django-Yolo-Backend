7816559bab8dcc3abbfc1eab4fffba5d72315c28
Erick F Esquilin <70776603+mrgbpjpy@users.noreply.github.com>
2025-08-26 16:56:34 -0400

    Update apps.py

--- videos/apps.py
+++ videos/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class VideosConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "videos"   # <-- if the folder is /app/videos. If it's inside mysite/, use "mysite.videos".
+    name = "videos"            # matches folder: /app/videos
+    verbose_name = "Video Processing"
