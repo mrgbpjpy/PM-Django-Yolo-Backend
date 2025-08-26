8c417104c42254e037dcb9ec18541e786bdab8a3
Erick F Esquilin <70776603+mrgbpjpy@users.noreply.github.com>
2025-08-26 16:52:23 -0400

    Update __init__.py

--- videos/__init__.py
+++ videos/__init__.py
@@ -1,5 +1,2 @@
-# from your project root locally
-python manage.py startapp videos  # (skip if it already exists)
-# make sure these two files exist:
-#   videos/__init__.py
-#   videos/apps.py
+# videos package marker
+
